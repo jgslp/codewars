52325baa1545b3d5ab85fec4b8a6c87aba355925
Jacqueline Gellick <jgellick@gmail.com>
2023-02-20 12:55:02 +0100

    Added completed codewars challenge

---
+++ 7kyu/greatest-common-divisor.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+function mygcd(x,y){
+    let xArr = [];
+    let yArr= [];
+    // find gcd for x
+    for (let i = x; i > 0; i--) {
+      if (x % i === 0) {
+        // push to array from highest to lowest
+        xArr.push(i);
+      }
+      xArr;
+    }
+    // loop through array and see if y is divisible each 
+    for (let j = 0; j < xArr.length; j++){
+      if (y % xArr[j] === 0) {
+        // if divisible, push to yArr
+        yArr.push(xArr[j])
+      }  
+    }
+    // return the first item in yArr
+    return yArr[0];
+  }
